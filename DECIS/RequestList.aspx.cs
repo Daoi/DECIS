f0eccabaa95fd1ff1830a151637ef058c09ea8eb
daoi <36860391+Daoi@users.noreply.github.com>
2021-09-08 13:46:45 -0400

    update request filtering/redirecting

--- DECIS/RequestList.aspx.cs
+++ DECIS/RequestList.aspx.cs
@@ -39,8 +39,12 @@ namespace DECIS
             //Get the row containing the clicked button
             LinkButton btn = (LinkButton)sender;
             GridViewRow row = (GridViewRow)btn.NamingContainer;
+            DataRow dr;
             //Recreate the Datarow the GVR is bound to
-            DataRow dr = (ViewState["RequestListDT"] as DataTable).Rows[row.DataItemIndex];
+            if (ViewState["Filter"] == null)
+                dr = (ViewState["RequestListDT"] as DataTable).Rows[row.DataItemIndex];
+            else
+                dr = (ViewState[$"{ViewState["Filter"].ToString()}"] as DataTable).Rows[row.DataItemIndex];
 
             Response.Redirect($"./RequestView.aspx?reqid={dr["RequestID"].ToString()}&type={dr["Type"].ToString()}");
         }
@@ -49,29 +53,44 @@ namespace DECIS
         protected void lb_Click(object sender, EventArgs e)
         {
             LinkButton lb = (LinkButton)sender;
+            DataTable result = null;
+            int status = 0;
             //Display
-            bool result = true;
             gvRequestList.Visible = true;
             lblGVMessage.Text = "";
             lblGVMessage.Visible = false;
+            ViewState["Filter"] = lb.ID;
             //Filter 
-            if (lb.ID == lbAll.ID )
+            if (lb.ID == lbAll.ID)
             {
                 gvRequestList.DataSource = dtRequestList;
                 gvRequestList.DataBind();
             }
-            else if(lb.ID == lbNew.ID)
-                result = DataTableFilter.Filter(dtRequestList, gvRequestList, r => r["RequestStatus"].ToString() == "New");
-            else if(lb.ID == lbPending.ID)
-                result = DataTableFilter.Filter(dtRequestList, gvRequestList, r => r["RequestStatus"].ToString() == "Pending");
-            else if(lb.ID == lbScheduled.ID)
-                result = DataTableFilter.Filter(dtRequestList, gvRequestList, r => r["RequestStatus"].ToString() == "Scheduled");
-            else if(lb.ID == lbFinished.ID)
-                result = DataTableFilter.Filter(dtRequestList, gvRequestList, r => r["RequestStatus"].ToString() == "Finished");
-            else
-                result = DataTableFilter.Filter(dtRequestList, gvRequestList, r => r["RequestStatus"].ToString() == "Cancelled");
-            //Didn't find anything that matched
-            if (!result)
+            else { 
+                if (lb.ID == lbNew.ID)
+                    status = 1;
+                else if (lb.ID == lbPending.ID)
+                    status = 2;
+                else if (lb.ID == lbScheduled.ID)
+                    status = 3;
+                else if (lb.ID == lbFinished.ID)
+                    status = 4;
+                else if (lb.ID == lbCancelled.ID)
+                    status = 5;
+
+                if (ViewState[$"{lb.ID}"] == null)
+                {
+                    ViewState[$"{lb.ID}"] = new GetAllRequestByStatus().ExecuteCommand(status);
+                    result = ViewState[$"{lb.ID}"] as DataTable;
+                }
+                else
+                    result = ViewState[$"{lb.ID}"] as DataTable;
+
+                gvRequestList.DataSource = result;
+                gvRequestList.DataBind();
+            }
+
+            if (result == null || result.Rows.Count == 0)
             {
                 lblGVMessage.Visible = true;
                 lblGVMessage.Text = $"Couldn't find any requests with status {lb.Text}";
