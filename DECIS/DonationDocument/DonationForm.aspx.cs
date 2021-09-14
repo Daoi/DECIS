6c927573d43248531a871d5bc9081adcc597bef1
daoi <36860391+Daoi@users.noreply.github.com>
2021-09-14 10:31:26 -0400

    document writing start

---
+++ DECIS/DonationDocument/DonationForm.aspx.cs
@@ -0,0 +1,31 @@
+﻿using DECIS.DataAccess.DataAccessors;
+using PuppeteerSharp;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DECIS.DonationDocument
+{
+    public partial class DonationForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["Flag"] = false;
+                gvEquipment.DataSource = new GetAllAssets().ExecuteCommand().Rows.OfType<DataRow>().ToList().Take(10).CopyToDataTable();
+                gvEquipment.DataBind();
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ DECIS/FileWriter/PDFWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace DECIS.FileWriter
+{
+    public class PDFWriter
+    {
+        //public static File Write(Page pg)
+        //{
+            
+        //}
+    }
+}
