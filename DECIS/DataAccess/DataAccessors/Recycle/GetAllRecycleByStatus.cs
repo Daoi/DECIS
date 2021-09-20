42e7240235faa714f4d97cf98eea8b67ab67baba
daoi <36860391+Daoi@users.noreply.github.com>
2021-09-20 11:56:06 -0400

    fix recycle list redirect

---
+++ DECIS/DataAccess/DataAccessors/Recycle/GetAllRecycleByStatus.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace DECIS.DataAccess.DataAccessors.Recycle
+{
+    public class GetAllRecycleByStatus : DataSupport, IData
+    {
+        public GetAllRecycleByStatus()
+        {
+            CommandText = "GetAllRecycleByStatus";
+            CommandType = CommandType.StoredProcedure;
+        }
+
+        public DataTable ExecuteCommand(int status)
+        {
+            Parameters = new MySqlParameter[1] { new MySqlParameter("RecycleStatus", status) };
+            ExecuteQuery eq = new ExecuteQuery(); //Create instance of class that handles command obj
+            return eq.ExecuteAdapter(this); //Run the command
+        }
+    }
+}
